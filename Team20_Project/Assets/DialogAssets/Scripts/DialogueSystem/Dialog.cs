d51c5085849797d509b10810ccb6c6f18e99545d
harun6384 <harun6384@gmail.com>
2022-06-02 15:54:56 +0300

    everything deleted

--- Team20_Project/Assets/DialogAssets/Scripts/DialogueSystem/Dialog.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:55dcc6aa17d13fe00babfc8b38b9f255d2eb001458c5bfcd4328a2388544f659
-size 428
--- Team20_Project/Assets/TurnBasedAssets/Scripts/StateMachines/BattleStateMachine.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:58f9ea0904edff0707eb1ebbea1dcccfa240c2e433a405dcc4b16f2248601659
-size 11023
